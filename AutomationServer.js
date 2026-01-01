3eff79505373976d2f135dc2f251e9ef1d3200c2
patzkie <pqtpatrick433@gmail.com>
2026-01-01 11:52:00 +0800

    Change default PORT from 3000 to 10000

--- AutomationServer.js
+++ AutomationServer.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 require("dotenv").config();
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 const app = express();
 app.use(express.json());
 
